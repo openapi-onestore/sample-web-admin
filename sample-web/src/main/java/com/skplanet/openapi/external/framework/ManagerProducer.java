66b031fcb47dfbe71589ac8edb99df712cb0ce7d
Jeff Kim <jeff.yg.kim@gmail.com>
2015-11-08 14:14:03 +0900

    sample code added (need to be changed)

--- sample-web/src/main/java/com/skplanet/openapi/external/framework/ManagerProducer.java
+++ sample-web/src/main/java/com/skplanet/openapi/external/framework/ManagerProducer.java
@@ -2,8 +2,16 @@ package com.skplanet.openapi.external.framework;
 
 public class ManagerProducer {
 	
+	public enum Environment{
+		LIVE,
+		SANDBOX
+	}
+	
 	public static AbstractManagerFactory getFactory(String propertyPath) {
 		return new ManagerFactory(propertyPath);
 	}
 	
+	public static AbstractManagerFactory getFactory(Environment env, String logPropetyFilePath) {
+		return null;
+	}
 }
